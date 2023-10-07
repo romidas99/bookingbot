362d3eed0e1f5bfb4abbbc16ab8965c1f572df22
Romidas <101974375+romidas99@users.noreply.github.com>
2023-10-07 17:35:38 +0800

    Add files via upload

---
+++ booking bot/app.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
